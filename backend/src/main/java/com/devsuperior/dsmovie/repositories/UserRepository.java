06cc1aee15bcc9b2759ef95d0540fb194cf005e7
Moises Guilherme <moisescomp@gmail.com>
2022-01-14 16:52:56 -0300

    Fix user

--- backend/src/main/java/com/devsuperior/dsmovie/repositories/UserRepository.java
+++ backend/src/main/java/com/devsuperior/dsmovie/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package com.devsuperior.dsmovie.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.devsuperior.dsmovie.entities.Movie;
 import com.devsuperior.dsmovie.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
